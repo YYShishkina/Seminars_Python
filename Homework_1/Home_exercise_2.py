5cbc5f5a7414ce81991eb9d690378bd19cab512a
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-12-11 08:06:18 +0300

    completed second exersice in first home work

--- Homework_1/Home_exercise_2.py
+++ Homework_1/Home_exercise_2.py
@@ -1,8 +1,4 @@
 for i in range (0, 2):
     for j in range (0,2):
         for k in range (0,2):
-            if (i or j or k) == i and j and k:
-                print ('for')
-                print(i,j,k)
-                print ('¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z is True')
-            
+            print(f' {i},{j},{k},{(not (i or j or k) == ((not i) and (not j) and (not k)))}') 
